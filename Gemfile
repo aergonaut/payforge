857348819ba948b0e14c002b234ed21b07f5c6e5
Chris Fung <aergonaut@gmail.com>
2014-07-07 09:57:02 -0700

    user spec

--- Gemfile
+++ Gemfile
@@ -46,7 +46,7 @@ gem 'octokit'
 
 gem 'bootstrap-sass'
 
-gem 'rails_12factor'
+gem 'rails_12factor', group: :production
 
 group :development, :test do
   gem 'rspec-rails'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  [:uid, :provider, :login, :name].each do |attr|
+    it { should respond_to attr }
+  end
 end
